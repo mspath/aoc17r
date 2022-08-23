d654362b7fa09eb706916af83acd0230309751d4
Markus Spath <markus@hackr.de>
2022-08-23 21:10:08 +0200

    Day 3 pt2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = 368078;
     let result_breakfast = breakfast(input);
@@ -12,19 +14,6 @@ enum Direction {
     North,
     West,
     South,
-}
-
-fn depth(cell: u32) -> u32 {
-    // 0, 1,   2,   3,   4,   5, ..
-    // 1, 3*3, 5*5, 7*7, 9*9, 11*11, ..
-    let mut level = 0;
-    if cell == 1 { return level; }
-
-    loop {
-        level = level + 1;
-        let max = (level * 2 + 1) * (level * 2 + 1);
-        if cell <= max { return level; }
-    }
 }
 
 fn get_distance(cell: u32) -> u32 {
@@ -77,8 +66,67 @@ fn breakfast(input: u32) -> u32 {
     distance
 }
 
+fn get_stress_value(input: u32) -> u32 {
+    let mut grid = HashMap::<(i32, i32), u32>::new();
+    grid.insert((0, 0), 1);
+
+    let mut max_west = 0;
+    let mut max_east = 0;
+    let mut max_north = 0;
+    let mut max_south = 0;
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut direction = Direction::East;
+
+    loop {
+        match direction {
+            Direction::East => {
+                x = x + 1;
+                if x > max_east {
+                    max_east = x;
+                    direction = Direction::North;
+                }
+            }
+            Direction::North => {
+                y = y - 1;
+                if y < max_north {
+                    max_north = y;
+                    direction = Direction::West;
+                }
+            }
+            Direction::West => {
+                x = x - 1;
+                if x < max_west {
+                    max_west = x;
+                    direction = Direction::South;
+                }  
+            }
+            Direction::South => {
+                y = y + 1;
+                if y > max_south {
+                    max_south = y;
+                    direction = Direction::East;
+                }
+            }
+        }
+        let nw = grid.get(&(x - 1, y - 1)).cloned().unwrap_or(0);
+        let n = grid.get(&(x, y - 1)).cloned().unwrap_or(0);
+        let ne = grid.get(&(x + 1, y - 1)).cloned().unwrap_or(0);
+        let w = grid.get(&(x - 1, y)).cloned().unwrap_or(0);
+        let e = grid.get(&(x + 1, y)).cloned().unwrap_or(0);
+        let sw = grid.get(&(x - 1, y + 1)).cloned().unwrap_or(0);
+        let s = grid.get(&(x, y + 1)).cloned().unwrap_or(0);
+        let se = grid.get(&(x + 1, y + 1)).cloned().unwrap_or(0);
+        let grid_value = nw + n + ne + w + e + sw + s + se;
+        grid.insert((x, y), grid_value);
+
+        // result is the first value larger than input
+        if grid_value > input { return grid_value; }
+    }
+}
+
 fn lunch(input: u32) -> u32 {
-    input - input
+    get_stress_value(input)
 }
 
 #[cfg(test)]
